d66d73d00e4c3e3c9d6b8a7a494f44c357d2330b
hieple <hieplevuc@gmail.com>
2024-07-01 13:23:45 +0700

    feat: Setup basic ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,11 +22,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} flex min-h-screen flex-col bg-primary-950 text-primary-100`}
+        className={`${josefin.className} relative flex min-h-screen flex-col bg-primary-950 text-primary-100`}
       >
         <Header></Header>
-        <div className="flex-1 px-8 py-12">
-          <main className="b mx-auto max-w-7xl">{children}</main>
+        <div className="grid flex-1 px-8 py-12">
+          <main className="mx-auto w-full max-w-7xl">{children}</main>
         </div>
         <footer>Copyright by The Wild Oasis</footer>
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,28 @@
+import Image from 'next/image';
+import bg from '@/public/bg.png';
 export default function Home() {
-  return <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>;
+  return (
+    <main className="mt-24">
+      <Image
+        src={bg}
+        fill
+        placeholder="blur"
+        quality={80}
+        className="object-cover object-top"
+        alt="Mountains and forests with two cabins"
+      />
+
+      <div className="relative z-10 text-center">
+        <h1 className="mb-10 text-8xl font-normal tracking-tight text-primary-50">
+          Welcome to paradise.
+        </h1>
+        <a
+          href="/cabins"
+          className="bg-accent-500 px-8 py-6 text-lg font-semibold text-primary-800 transition-all hover:bg-accent-600"
+        >
+          Explore luxury cabins
+        </a>
+      </div>
+    </main>
+  );
 }
